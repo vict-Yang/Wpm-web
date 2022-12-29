feaf5490651328968a6ba9d30e6fb5d2fead901b
vincent <b10902042@csie.ntu.edu.tw>
2022-12-29 10:21:12 +0800

    add: refactor some style the char can be styled more easily

--- frontend/src/components/Character.js
+++ frontend/src/components/Character.js
@@ -1,70 +1,38 @@
 import { Typography } from "@mui/material";
-const IncorrectCharacter = ({ char }) => {
-  return (
-    <Typography
-      sx={{
-        color: "#CC0000",
-        textDecoration: "underline",
-        textDecorationStyle: "wavy",
-        display: "inline-block",
-        whiteSpace: "pre",
-      }}
-    >
-      {char}
-    </Typography>
-  );
+import { styled } from "@mui/system";
+const charBasicStype = {
+  display: "inline-block",
+  whiteSpace: "pre",
 };
-const CorrectCharacter = ({ char }) => {
-  return (
-    <Typography
-      sx={{
-        color: "#008000",
-        display: "inline-block",
-        whiteSpace: "pre",
-      }}
-    >
-      {char}
-    </Typography>
-  );
-};
-const NextCharacter = ({ char }) => {
-  return (
-    <Typography
-      color={"textSecondary"}
-      sx={{
-        display: "inline-block",
-        whiteSpace: "pre",
-        background: "#A8A8A8"
-      }}
-    >
-      {char}
-    </Typography>
-  );
-};
-const NotReachedCharacter = ({ char }) => {
-  return (
-    <Typography
-      color={"textSecondary"}
-      sx={{
-        display: "inline-block",
-        whiteSpace: "pre"
-      }}
-    >
-      {char}
-    </Typography>
-  );
-};
+const IncorrectCharacter = styled(Typography)({
+  ...charBasicStype,
+  color: "#CC0000",
+  textDecoration: "underline",
+});
+const CorrectCharacter =  styled(Typography)({
+  ...charBasicStype,
+  color: "#008000",
+
+});
+const NextCharacter = styled(Typography)({
+  ...charBasicStype,
+  background: "#A8A8A8",
+  
+});
+const NotReachCharacter = styled(Typography)({
+  ...charBasicStype,
+})
 const Character = ({ char, id, charsTyped }) => {
   let ShowCharacter;
   if (id == charsTyped.length) {
     ShowCharacter = NextCharacter;
   } else if (id >= charsTyped.length) {
-    ShowCharacter = NotReachedCharacter;
+    ShowCharacter = NotReachCharacter;
   } else if (charsTyped[id] === char) {
     ShowCharacter = CorrectCharacter;
   } else {
     ShowCharacter = IncorrectCharacter;
   }
-  return <ShowCharacter char={char}></ShowCharacter>;
+  return <ShowCharacter variant="h5" color={"textSecondary"}>{char}</ShowCharacter>;
 };
 export { Character };
